71b0f1c3f7a475f5a00b75d5ac886205d7251326
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-05 22:59:52 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,40 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "minecraft" {
-  ami           = "ami-01cd4de4363ab6ee8"  # 원하는 AMI ID로 변경
-  instance_type = "t3.small"               # 인스턴스 유형 선택
-  key_name      = var.key_name  # EC2에서 생성한 키 페어 이름
-
-  security_groups = [aws_security_group.minecraft.name]
-
-  tags = {
-    Name = "minecraft-server"
-  }
+data "aws_instance" "minecraft" {
+  instance_id = "i-0ba311bacff7784d4"
 }
 
-resource "aws_security_group" "minecraft" {
-  name        = "minecraft-security-group"
-  description = "Security group for Minecraft server"
-
-  ingress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "existing_security_group" {
+  id = "sg-03f416246cf746bf8"
 }
