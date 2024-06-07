85e124d2a280a569223679d586a824189c97613e
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-06 22:35:31 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
+data "aws_security_group" "existing" {
+  name = "minecraft-security-group"
+}
+
 resource "aws_security_group" "minecraft" {
+  count       = length(data.aws_security_group.existing) == 0 ? 1 : 0
   name        = "minecraft-security-group"
   description = "Security group for Minecraft server"
-
-  // SSH 및 Minecraft 포트에 대한 인바운드 규칙 추가
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 25565
@@ -21,11 +18,17 @@ resource "aws_security_group" "minecraft" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  // 모든 트래픽에 대한 아웃바운드 규칙 추가
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -34,7 +37,7 @@ resource "aws_instance" "minecraft" {
   ami           = "ami-01cd4de4363ab6ee8"
   instance_type = "t3.small"
   key_name      = "lab6"
-  security_groups = [aws_security_group.minecraft.name]
+  security_groups = [data.aws_security_group.existing.name]
 
   tags = {
     Name = "MinecraftServer"
