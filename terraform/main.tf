6737aa96921106f17bb36788cba3a223c92c51ac
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-07 15:01:38 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "minecraft" {
 
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
