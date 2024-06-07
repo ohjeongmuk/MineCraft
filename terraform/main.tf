5f887d4e648843ca9e6017a881bfb70b4c9a611a
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-07 00:44:11 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,32 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "minecraft" {
-  name        = "minecraft-security-group"
-  description = "Security group for Minecraft server"
-
-  ingress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 data "aws_security_group" "existing" {
   id = "sg-0f22cfa2e6de7ffe6"  # 기존 보안 그룹의 ID 사용
 }
@@ -36,7 +10,7 @@ resource "aws_instance" "minecraft" {
   ami           = "ami-01cd4de4363ab6ee8"
   instance_type = "t3.small"
   key_name      = "lab6"
-  security_groups = length(data.aws_security_group.existing) == 0 ? [aws_security_group.minecraft.name] : [data.aws_security_group.existing.id]
+  security_groups = [data.aws_security_group.existing.id]
 
   tags = {
     Name = "MinecraftServer"
