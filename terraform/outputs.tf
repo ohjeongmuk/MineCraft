2800764353780bf65f1d6f394e453d1f44d00b6c
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-06 21:00:50 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "instance_ip" {
   value = aws_instance.minecraft.public_ip
 }
 
-output "security_group_id" {
-  value = aws_security_group.existing_security_group.id
-}
+#output "security_group_id" {
+#  value = aws_security_group.existing_security_group.id
+#}
