c1523fa43dc7bfc0fd2b84585ef7238346dac956
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-06 20:54:10 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# "<Provider>_<type>" "<name>" => 생성
 resource "aws_instance" "minecraft" {
   ami           = "ami-01cd4de4363ab6ee8"  # 사용할 AMI ID로 변경
   instance_type = "t3.small"               # 사용할 인스턴스 유형으로 변경
