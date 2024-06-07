a7ef1e53081ea24eac9fbc08407f3a0cd054e5af
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-06 23:55:48 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,43 +3,15 @@ provider "aws" {
 }
 
 data "aws_security_group" "existing" {
-  name = "minecraft-security-group"
-}
-
-resource "aws_security_group" "minecraft" {
-  count       = length(data.aws_security_group.existing) == 0 ? 1 : 0
-  name        = "minecraft-security-group"
-  description = "Security group for Minecraft server"
-
-  ingress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  id = "sg-0f22cfa2e6de7ffe6"  # 기존 보안 그룹의 ID 사용
 }
 
 resource "aws_instance" "minecraft" {
+  count         = length(data.aws_security_group.existing) == 0 ? 1 : 0
   ami           = "ami-01cd4de4363ab6ee8"
   instance_type = "t3.small"
   key_name      = "lab6"
-
-  # 보안 그룹 설정 변경
-  security_groups = length(data.aws_security_group.existing) == 0 ? [aws_security_group.minecraft[0].name] : [data.aws_security_group.existing[0].name]
+  security_groups = length(data.aws_security_group.existing) == 0 ? [aws_security_group.minecraft.name] : [data.aws_security_group.existing.id]
 
   tags = {
     Name = "MinecraftServer"
@@ -49,4 +21,3 @@ resource "aws_instance" "minecraft" {
     prevent_destroy = true  # 인스턴스를 삭제하지 않도록 설정
   }
 }
-
