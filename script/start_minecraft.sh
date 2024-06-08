6cce50c3b799d59c69008c3d7e8bb46737fbb72e
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 16:24:53 -0700

    Update start_minecraft.sh

--- script/start_minecraft.sh
+++ script/start_minecraft.sh
@@ -10,33 +10,36 @@ MINECRAFTSERVERURL=https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed
 # Download Java
 sudo yum install -y java-17-amazon-corretto-headless
 # Install MC Java server in a directory we create
-sudo adduser minecraft
-sudo mkdir /opt/minecraft/
-sudo mkdir /opt/minecraft/server/
-sudo chown -R minecraft:minecraft /opt/minecraft/
-sudo cd /opt/minecraft/server
+adduser minecraft
+mkdir /opt/minecraft/
+mkdir /opt/minecraft/server/
+cd /opt/minecraft/server
 
 # Download server jar file from Minecraft official website
-sudo wget $MINECRAFTSERVERURL
+wget $MINECRAFTSERVERURL
 
 # Generate Minecraft server files and create script
-sudo java -Xmx1300M -Xms1300M -jar server.jar nogui
+chown -R minecraft:minecraft /opt/minecraft/
+java -Xmx1300M -Xms1300M -jar server.jar nogui
 sleep 40
-sudo sed -i 's/false/true/p' eula.txt
-sudo touch start
-sudo printf '#!/bin/bash\njava -Xmx1300M -Xms1300M -jar server.jar nogui\n' >> start
-sudo chmod +x start
-sudo sleep 1
-sudo touch stop
-sudo printf '#!/bin/bash\nkill -9 $(ps -ef | pgrep -f "java")' >> stop
-sudo chmod +x stop
-sudo sleep 1
+sed -i 's/false/true/p' eula.txt
+touch start
+printf '#!/bin/bash\njava -Xmx1300M -Xms1300M -jar server.jar nogui\n' >> start
+chmod +x start
+sleep 1
+touch stop
+printf '#!/bin/bash\nkill -9 $(ps -ef | pgrep -f "java")\n' >> stop
+chmod +x stop
+sleep 1
 
 # Create SystemD Script to run Minecraft server jar on reboot
-sudo touch /etc/systemd/system/minecraft.service
-sudo printf '[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> /etc/systemd/system/minecraft.service
+cd /etc/systemd/system/
+sudo touch minecraft.service
+sudo chmod 644 minecraft.service
+printf '[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> minecraft.service
 sudo systemctl daemon-reload
+sudo systemctl unmask minecraft.service
 sudo systemctl enable minecraft.service
-sudo systemctl start minecraft.service
+sudo systemctl restart minecraft.service
 
 # End script
