ecfc5e3e80939e70fe4ebef02acb2a8778f5159c
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 13:41:42 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,50 +1,32 @@
-provider "aws" {
-  region = "us-west-2"
+# VPC 설정
+resource "aws_vpc" "minecraft_vpc" {
+  cidr_block = "10.0.0.0/16" # 사용 가능한 CIDR 블록 선택
 }
 
+# 서브넷 설정
+resource "aws_subnet" "minecraft_subnet" {
+  vpc_id            = aws_vpc.minecraft_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a" # 원하는 가용성 영역 선택
+}
+
+# 보안 그룹 설정 (기존 보안 그룹 사용)
 data "aws_security_group" "existing" {
-  name = "MineCraft"
+  name = "MineCraft" # 기존 보안 그룹 이름
 }
 
-resource "aws_security_group" "minecraft" {
-  count       = length(data.aws_security_group.existing) == 0 ? 1 : 0
-  name        = "MineCraft"
-  description = "Security group for Minecraft server"
-
-  ingress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
+# Minecraft 인스턴스 설정
 resource "aws_instance" "minecraft" {
-  ami           = "ami-01cd4de4363ab6ee8"
-  instance_type = "t3.small"
-  key_name      = "lab6"
-  availability_zone = "us-west-2a"  # 여기서 원하는 AZ를 지정합니다.
-  subnet_id = "subnet-0dc899575612c8714"
+  ami           = "ami-01cd4de4363ab6ee8" # 사용할 AMI 선택
+  instance_type = "t3.small" # 원하는 인스턴스 유형 선택
+  key_name      = "lab6" # SSH 키 이름
+  subnet_id     = aws_subnet.minecraft_subnet.id
 
   tags = {
     Name = "MinecraftServer"
   }
 
   lifecycle {
-    prevent_destroy = true  # 인스턴스를 삭제하지 않도록 설정
+    prevent_destroy = true # 인스턴스 삭제 방지
   }
 }
