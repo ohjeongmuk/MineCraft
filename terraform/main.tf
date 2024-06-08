a8726bb9209506486fc4aea5921305282da789a9
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 15:13:26 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,44 +2,50 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "minecraft" {
+data "aws_security_group" "existing" {
   name = "MineCraft"
+}
+
+resource "aws_security_group" "minecraft" {
+  count       = length(data.aws_security_group.existing) == 0 ? 1 : 0
+  name        = "MineCraft"
   description = "Security group for Minecraft server"
 
   ingress {
-    from_port = 25565
-    to_port = 25565
-    protocol = "tcp"
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "minecraft" {
-  ami = "ami-01cd4de4363ab6ee8"
+  ami           = "ami-01cd4de4363ab6ee8"
   instance_type = "t3.small"
-  key_name = "lab6"
-  availability_zone = "us-west-2a" # 여기서 원하는 AZ를 지정합니다.
-  subnet_id = "subnet-0dc899575612c8714"
+  key_name      = "lab6"
+  security_groups = [data.aws_security_group.existing.name]
+  availability_zone = "us-west-2a"  # 원하는 가용 영역을 여기에 지정합니다.
+  subnet_id     = "subnet-1234567890abcdef0"  # 원하는 서브넷의 ID를 여기에 지정합니다.
 
   tags = {
     Name = "MinecraftServer"
   }
 
   lifecycle {
-    prevent_destroy = true # 인스턴스를 삭제하지 않도록 설정
+    prevent_destroy = true  # 인스턴스를 삭제하지 않도록 설정
   }
 }
