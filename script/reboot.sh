824892ef30ecb4bdab08a5bac978ca20787576a6
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 22:47:42 -0700

    Update reboot.sh

--- script/reboot.sh
+++ script/reboot.sh
@@ -4,13 +4,13 @@
 sudo yum install -y cronie
 
 # 크론 스크립트 작성
-cat <<EOF > startup.sh
+cat <<EOF > /opt/minecraft/server/startup.sh
 #!/bin/bash
-sudo ./start
+sudo /opt/minecraft/server/start
 EOF
 
 # 크론 스크립트에 실행 권한 부여
-chmod +x startup.sh
+chmod +x /opt/minecraft/server/startup.sh
 
 # 크론 작업 등록
-(crontab -l 2>/dev/null; echo "@reboot /path/to/startup.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot /opt/minecraft/server/startup.sh") | crontab -
