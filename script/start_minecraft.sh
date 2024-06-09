c1d43105fdd72e77cf4333a3e06835c5730a9241
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 17:57:57 -0700

    Update start_minecraft.sh

--- script/start_minecraft.sh
+++ script/start_minecraft.sh
@@ -1,10 +1,51 @@
 #!/bin/bash
 
-# Create script.bash
-touch script.bash
+# Create script.bash and add the content
+cat <<EOF > script.bash
+#!/bin/bash
 
-# Add the script content to script.bash
-printf '#!/bin/bash\n\n# *** INSERT SERVER DOWNLOAD URL BELOW ***\n# Do not add any spaces between your link and the "=", otherwise it won\'t work. EG: MINECRAFTSERVERURL=https://urlexample\n\nMINECRAFTSERVERURL=https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar\n\n# Download Java\nsudo yum install -y java-17-amazon-corretto-headless\n\n# Install MC Java server in a directory we create\nadduser minecraft\nmkdir /opt/minecraft/\nmkdir /opt/minecraft/server/\ncd /opt/minecraft/server\n\n# Download server jar file from Minecraft official website\nwget $MINECRAFTSERVERURL\n\n# Generate Minecraft server files and create script\nchown -R minecraft:minecraft /opt/minecraft/\njava -Xmx1300M -Xms1300M -jar server.jar nogui\nsleep 40\nsed -i \'s/false/true/p\' eula.txt\ntouch start\nprintf \'#!/bin/bash\njava -Xmx1300M -Xms1300M -jar server.jar nogui\n\' >> start\nchmod +x start\nsleep 1\ntouch stop\nprintf \'#!/bin/bash\nkill -9 $(ps -ef | pgrep -f "java")\n\' >> stop\nchmod +x stop\nsleep 1\n\n# Create SystemD Script to run Minecraft server jar on reboot\ncd /etc/systemd/system/\ntouch minecraft.service\nprintf \'[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target\' >> minecraft.service\nsudo systemctl daemon-reload\nsudo systemctl enable minecraft.service\nsudo systemctl start minecraft.service\n\n# End script\n' > script.bash
+# *** INSERT SERVER DOWNLOAD URL BELOW ***
+# Do not add any spaces between your link and the "=", otherwise it won't work. EG: MINECRAFTSERVERURL=https://urlexample
+
+
+MINECRAFTSERVERURL=https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
+
+
+# Download Java
+sudo yum install -y java-17-amazon-corretto-headless
+# Install MC Java server in a directory we create
+adduser minecraft
+mkdir /opt/minecraft/
+mkdir /opt/minecraft/server/
+cd /opt/minecraft/server
+
+# Download server jar file from Minecraft official website
+wget \$MINECRAFTSERVERURL
+
+# Generate Minecraft server files and create script
+chown -R minecraft:minecraft /opt/minecraft/
+java -Xmx1300M -Xms1300M -jar server.jar nogui
+sleep 40
+sed -i 's/false/true/p' eula.txt
+touch start
+printf '#!/bin/bash\njava -Xmx1300M -Xms1300M -jar server.jar nogui\n' >> start
+chmod +x start
+sleep 1
+touch stop
+printf '#!/bin/bash\nkill -9 \$(ps -ef | pgrep -f "java")' >> stop
+chmod +x stop
+sleep 1
+
+# Create SystemD Script to run Minecraft server jar on reboot
+cd /etc/systemd/system/
+touch minecraft.service
+printf '[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> minecraft.service
+sudo systemctl daemon-reload
+sudo systemctl enable minecraft.service
+sudo systemctl start minecraft.service
+
+# End script
+EOF
 
 # Make script.bash executable
 chmod +x script.bash
