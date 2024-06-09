20fd674d23d2c6ec8cf7f1e13a556250ec5ce747
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 21:42:10 -0700

    Update start_minecraft.sh

--- script/start_minecraft.sh
+++ script/start_minecraft.sh
@@ -3,9 +3,7 @@
 # *** INSERT SERVER DOWNLOAD URL BELOW ***
 # Do not add any spaces between your link and the "=", otherwise it won't work. EG: MINECRAFTSERVERURL=https://urlexample
 
-
 MINECRAFTSERVERURL=https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
-
 
 # Download Java
 sudo yum install -y java-17-amazon-corretto-headless
@@ -20,22 +18,37 @@ wget $MINECRAFTSERVERURL
 
 # Generate Minecraft server files and create script
 chown -R minecraft:minecraft /opt/minecraft/
-java -Xmx1300M -Xms1300M -jar server.jar nogui
+sudo -u minecraft java -Xmx1300M -Xms1300M -jar server.jar nogui
 sleep 40
-sed -i 's/false/true/p' eula.txt
+
+# Agree to EULA
+sudo -u minecraft sed -i 's/eula=false/eula=true/' /opt/minecraft/server/eula.txt
+
+# Create start script
 touch start
-printf '#!/bin/bash\njava -Xmx1300M -Xms1300M -jar server.jar nogui\n' >> start
+echo '#!/bin/bash' > start
+echo 'java -Xmx1300M -Xms1300M -jar server.jar nogui' >> start
 chmod +x start
-sleep 1
+
+# Create stop script
 touch stop
-printf '#!/bin/bash\nkill -9 $(ps -ef | pgrep -f "java")' >> stop
+echo '#!/bin/bash' > stop
+echo 'kill -9 $(ps -ef | pgrep -f "java")' >> stop
 chmod +x stop
-sleep 1
 
 # Create SystemD Script to run Minecraft server jar on reboot
 cd /etc/systemd/system/
 touch minecraft.service
-printf '[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> minecraft.service
+echo '[Unit]' > minecraft.service
+echo 'Description=Minecraft Server on start up' >> minecraft.service
+echo 'Wants=network-online.target' >> minecraft.service
+echo '[Service]' >> minecraft.service
+echo 'User=minecraft' >> minecraft.service
+echo 'WorkingDirectory=/opt/minecraft/server' >> minecraft.service
+echo 'ExecStart=/opt/minecraft/server/start' >> minecraft.service
+echo 'StandardInput=null' >> minecraft.service
+echo '[Install]' >> minecraft.service
+echo 'WantedBy=multi-user.target' >> minecraft.service
 sudo systemctl daemon-reload
 sudo systemctl enable minecraft.service
 sudo systemctl start minecraft.service
