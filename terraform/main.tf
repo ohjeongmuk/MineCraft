6bf2eaed9b9a3874b15e39197cdcc39b39c1be6b
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 18:42:58 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,13 @@ resource "aws_instance" "minecraft" {
     ]
   }
 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(var.ssh_private_key_path)  # 액션에서 전달된 SSH 개인 키 파일 경로 사용
+    host        = self.public_ip
+  }
+
   tags = {
     Name = "MinecraftServer"
   }
