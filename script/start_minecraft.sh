3bd885e45be3c637ca8fc17b142a7e63ecf9a61c
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-08 18:20:32 -0700

    Update start_minecraft.sh

--- script/start_minecraft.sh
+++ script/start_minecraft.sh
@@ -14,31 +14,31 @@ MINECRAFTSERVERURL=https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed
 # Download Java
 sudo yum install -y java-17-amazon-corretto-headless
 # Install MC Java server in a directory we create
-adduser minecraft
-mkdir /opt/minecraft/
-mkdir /opt/minecraft/server/
+sudo adduser minecraft
+sudo mkdir /opt/minecraft/
+sudo mkdir /opt/minecraft/server/
 cd /opt/minecraft/server
 
 # Download server jar file from Minecraft official website
 wget \$MINECRAFTSERVERURL
 
 # Generate Minecraft server files and create script
-chown -R minecraft:minecraft /opt/minecraft/
-java -Xmx1300M -Xms1300M -jar server.jar nogui
-sleep 40
+sudo chown -R minecraft:minecraft /opt/minecraft/
+sudo java -Xmx1300M -Xms1300M -jar server.jar nogui
+sudo sleep 40
 sed -i 's/false/true/p' eula.txt
-touch start
+sudo touch start
 printf '#!/bin/bash\njava -Xmx1300M -Xms1300M -jar server.jar nogui\n' >> start
-chmod +x start
+sudo chmod +x start
 sleep 1
-touch stop
+sudo touch stop
 printf '#!/bin/bash\nkill -9 \$(ps -ef | pgrep -f "java")' >> stop
-chmod +x stop
+sudo chmod +x stop
 sleep 1
 
 # Create SystemD Script to run Minecraft server jar on reboot
 cd /etc/systemd/system/
-touch minecraft.service
+sudo touch minecraft.service
 printf '[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> minecraft.service
 sudo systemctl daemon-reload
 sudo systemctl enable minecraft.service
