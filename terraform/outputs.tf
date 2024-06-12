ab7326c2e14bbdfc1d610b4a69d533939b01ccba
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-11 17:00:10 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "instance_ip" {
   value = aws_instance.minecraft.public_ip
 }
+
+# 키 페어 생성 후 private key를 출력하여 사용할 수 있도록 함
+output "private_key" {
+  value = tls_private_key.minecraft.private_key_pem
+  sensitive = true
+}
