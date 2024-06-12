3d7eec78eb84482b8d1c74a8d6a0dffdfd059398
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-11 17:39:01 -0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "key_name" {
   description = "The name of the SSH key pair to use for the EC2 instance"
   default     = "minecraft_key"  # 키페어의 기본 이름을 설정합니다. 필요에 따라 변경할 수 있습니다.
 }
-
-variable "public_key" {
-  description = "The public key to use for the instance"
-  type        = string
-}
