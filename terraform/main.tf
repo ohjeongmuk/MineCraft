39ac45756e113b6af0f524093cc9cc9788280f67
ohjeongmuk <37374081+ohjeongmuk@users.noreply.github.com>
2024-06-11 17:35:19 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-
-# TLS 키 페어 생성
-resource "tls_private_key" "minecraft" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
 # AWS 키 페어 생성
 resource "aws_key_pair" "minecraft" {
   key_name   = "minecraft-key"
-  public_key = tls_private_key.minecraft.public_key_openssh
+  public_key = var.public_key
 }
 
 # 새로운 보안 그룹 생성
